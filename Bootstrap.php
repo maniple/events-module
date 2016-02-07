117336a05228085df2f6edd217e56baa115e34aa
xemlock <xemlock@gmail.com>
2016-02-07 13:22:33 +0100

    setup entity manager

--- Bootstrap.php
+++ Bootstrap.php
@@ -1,4 +1,11 @@
 <?php
 
 class EventsModule_Bootstrap extends Maniple_Application_Module_Bootstrap
-{}
+{
+    protected function _initEntityManager()
+    {
+        /** @var ManipleCore\Doctrine\Config $config */
+        $config = $this->getApplication()->getResource('EntityManager.config');
+        $config->addPath(__DIR__ . '/library/Entity');
+    }
+}
--- library/Entity/Event.php
+++ library/Entity/Event.php
@@ -4,14 +4,29 @@ namespace Maniple\EventsModule\Entity;
 
 /**
  * @Entity
+ * @Table(name="events")
  */
 class Event
 {
+    /**
+     * @Id
+     * @Column(name="event_id", type="integer")
+     * @GeneratedValue(strategy="AUTO")
+     */
     protected $_id;
 
+    /**
+     * @Column(name="title", type="string", length=255, nullable=false)
+     */
     protected $_title;
 
+    /**
+     * @Column(name="start_time", type="epoch", nullable=false)
+     */
     protected $_startTime;
 
+    /**
+     * @Column(name="end_time", type="epoch", nullable=false)
+     */
     protected $_endTime;
 }
